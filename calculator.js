34285b93b9e7a5ad86b421230350b198085b03e0
Kriti98ranjan <kritiranjan196@gmail.com>
2026-06-06 23:45:22 +0530

    update console message

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 // calculator.js
 function add(a, b) {
-  //console.log("Adding from Clone 2");
-  //console.log("Adding", a, "and", b);
+  //console.log("Adding from Clone-2");
+  //console.log("Adding from Clone-1");
   return a + b;
 }
 
