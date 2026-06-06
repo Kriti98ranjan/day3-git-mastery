dd9323266726f253a41c268a5219b32ed33e9a2a
Kriti98ranjan <kritiranjan196@gmail.com>
2026-06-06 18:58:10 +0530

    add work in progress comment

--- calculator.js
+++ calculator.js
@@ -26,3 +26,5 @@ module.exports = { add, subtract, multiply, divide, square };// version 1.1
 function modulo(a, b){ return a % b; }
 console.log("Calculator loaded");
 // final version
+
+// work in progress
